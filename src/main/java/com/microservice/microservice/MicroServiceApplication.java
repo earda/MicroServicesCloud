be99fe2177936bb1e447fcf891525f81880f2b53
Erdem <earda230@gmail.com>
2023-06-18 11:36:25 +0300

    Spring initialized
    Spring initialized

---
+++ src/main/java/com/microservice/microservice/MicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApplication.class, args);
+	}
+
+}
