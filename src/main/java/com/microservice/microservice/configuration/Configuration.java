2375cd8ae2253953efa31eafaff074277f235491
Erdem <earda230@gmail.com>
2023-06-19 19:16:34 +0300

    Lombok added
    Lombok added

--- src/main/java/com/microservice/microservice/configuration/Configuration.java
+++ src/main/java/com/microservice/microservice/configuration/Configuration.java
@@ -1,28 +1,16 @@
 package com.microservice.microservice.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
 @ConfigurationProperties("boundary-service")
 public class Configuration {
 
     private int minimum;
-    private int maximum;
-
-    public int getMinimum() {
-        return minimum;
-    }
-
-    public void setMinimum(int minimum) {
-        this.minimum = minimum;
-    }
-
-    public int getMaximum() {
-        return maximum;
-    }
-
-    public void setMaximum(int maximum) {
-        this.maximum = maximum;
-    }
+    private int maximum; 
 }
